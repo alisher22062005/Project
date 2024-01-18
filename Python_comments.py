3a5bde26b018af5a940ee4c66c2767cb4b287007
alisher22062005 <156660415+alisher22062005@users.noreply.github.com>
2024-01-18 17:16:46 +0600

    Add files via upload

---
+++ Python_comments.py
@@ -0,0 +1,34 @@
+#print("Hello, World!") #example1
+print("Cheers, Mate!")
+
+
+#This is a comment    #example2
+#written in
+#more than just one line
+print("Hello, World!")
+
+
+#example3
+"""
+This is a comment   
+written in
+more than just one line
+"""
+print("Hello, World!")
+
+
+#exercise1
+#This is a comment   
+
+#exercise2
+"""
+This is a comment
+written in 
+more than just one line
+"""
+ 
+
+
+
+
+
