616fc7f3b7eb046fa4d728edeb4cfe1fd1bb9acb
alisher22062005 <156660415+alisher22062005@users.noreply.github.com>
2024-01-18 17:35:36 +0600

    Add files via upload

---
+++ Python_Variables.py
@@ -0,0 +1,64 @@
+#example1
+x = 5
+y = "John"
+print(x)
+print(y)
+
+#example2
+x = 4       # x is of type int
+x = "Sally" # x is now of type str
+print(x)
+
+#example3
+x = str(3)    # x will be '3'
+y = int(3)    # y will be 3
+z = float(3)  # z will be 3.0
+
+#exmaple3
+x = "John"
+# is the same as
+x = 'John'
+
+#example4
+x = 5
+y = "John"
+print(type(x))
+print(type(y))
+
+#example5
+a = 4
+A = "Sally"
+#A will not overwrite a
+
+
+#exercice1
+carname="Volvo"
+
+
+#exercise2
+x=50
+
+#exercise3
+x=5
+y=10
+print(x+y)
+
+#exercise4
+x=5
+y=10
+z=x+y
+print(z)
+
+#exercise5
+x,y,z="Orange", "Banana", "Cherry"
+
+
+#exercise6
+x=y=z="Orange"
+
+#exercise7
+def myfunc():
+    global x
+    x="fantastic"
+  
+
