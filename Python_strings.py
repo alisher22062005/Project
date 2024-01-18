a3e1213beb950f8d618be161ab8c6f076e06175d
alisher22062005 <156660415+alisher22062005@users.noreply.github.com>
2024-01-18 19:00:58 +0600

    Add files via upload

---
+++ Python_strings.py
@@ -0,0 +1,35 @@
+#example1
+x = "Hello World"
+print(len(x))
+
+#example2
+txt = "Hello World"
+x =txt[0]
+
+#example3
+txt = "Hello World"
+x =txt[2:5]
+
+#example4
+txt = " Hello World "
+x = txt.strip()
+
+#example5
+txt = "Hello World"
+txt = txt.upper()
+
+#example6
+txt = "Hello World"
+txt = txt.lower()
+
+#example7
+txt = "Hello World"
+txt = txt.replace("H","J")
+
+#example8
+
+age = 36
+txt = "My name is John, and I am {}" 
+print(txt.format(age))
+
+
