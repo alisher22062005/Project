8278b6db3bd0147db427b638b17d31689e857d9a
alisher22062005 <156660415+alisher22062005@users.noreply.github.com>
2024-01-18 18:23:13 +0600

    Add files via upload

---
+++ Pyhton_Data_Types.py
@@ -0,0 +1,29 @@
+#exercise1
+x=5 #int
+print(type(x))
+
+
+#exercise2
+x="Hello World" #str
+print(type(x))
+
+#exercise3
+x=20.5 #float
+print(type(x))
+
+#exercise4
+x = ["apple", "banana", "cherry"] #list
+print(type(x))
+
+#exercise5
+x = ("apple", "banana", "cherry")#tuple
+print(type(x))
+
+#exercise6
+x = {"name" : "John", "age" : 36}#dict
+print(type(x))
+
+#exercise7
+x = True#bool
+print(type(x))
+
