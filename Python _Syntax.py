7234f89de7a1a092847c71baf6eacde782a48508
alisher22062005 <156660415+alisher22062005@users.noreply.github.com>
2024-01-18 17:07:10 +0600

    Add files via upload

---
+++ Python _Syntax.py
@@ -0,0 +1,12 @@
+if 5>2: # example1
+    print("Five is greater than two!")
+
+if 5>2: # example2
+    print("Five is greater than two!")
+if 5>2:
+    print("Five is greater thifan two!")
+
+print("Hello World") #exercise1
+
+if 5>2:#exercise2
+    print("YES")
